22c40255f7917a3ec3a24d44a3e13dc8e8fc96ef
Shubham Jaiswal <shubhamjaiswal3417@gmail.com>
2021-06-02 13:49:17 +0530

    Add files via upload

---
+++ CONVERT PDF FILE TEXT TO AUDIO USING PYTHON/main.py
@@ -0,0 +1,23 @@
+# CONVERT PDF FILE TEXT TO AUDIO USING PYTHON
+
+# importing the modules
+import PyPDF2
+import pyttsx3
+
+# path of the PDF file
+path = open('file.pdf', 'rb')
+
+# creating a PDFFileReader object
+pdfReader = PyPDF2.PdfFileReader(path)
+
+# the page with which you want to start
+# this will read the page of 1st page.
+from_page = pdfReader.getPage(1)
+
+# extracting the text from the PDF
+text = from_page.extractText()
+
+# reading the text
+speak = pyttsx3.init()
+speak.say(text)
+speak.runAndWait()
